331c949b807eb2e04c57769b62088142e5ae795a
yjgaia <yjdev88@gmail.com>
2024-11-17 21:37:26 +0900

    Refactor readContract method signatures in UniversalWalletConnector and WalletSessionManager to improve type handling for contract function return types

--- lib/WalletSessionManager.d.ts
+++ lib/WalletSessionManager.d.ts
@@ -12,7 +12,7 @@ declare class WalletSessionManager extends EventContainer<{
     connect(): Promise<void>;
     disconnect(): void;
     getBalance(chainId: number, walletAddress: `0x${string}`): Promise<bigint>;
-    readContract<const abi extends Abi | readonly unknown[], functionName extends ContractFunctionName<abi, "pure" | "view">, args extends ContractFunctionArgs<abi, "pure" | "view", functionName>>(parameters: ReadContractParameters<abi, functionName, args, Config>): Promise<import("viem").ContractFunctionReturnType<abi, "pure" | "view", functionName, args>>;
+    readContract<const abi extends Abi | readonly unknown[], functionName extends ContractFunctionName<abi, "pure" | "view">, args extends ContractFunctionArgs<abi, "pure" | "view", functionName>>(parameters: ReadContractParameters<abi, functionName, args, Config>): Promise<import("viem").ContractFunctionReturnType<abi, "view" | "pure", functionName, args>>;
     writeContract<const abi extends Abi | readonly unknown[], functionName extends ContractFunctionName<abi, "nonpayable" | "payable">, args extends ContractFunctionArgs<abi, "nonpayable" | "payable", functionName>, chainId extends Config["chains"][number]["id"]>(parameters: WriteContractParameters<abi, functionName, args, Config, chainId>): Promise<void>;
 }
 declare const _default: WalletSessionManager;
