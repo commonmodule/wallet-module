7d2579225fc69bd6a805dc74d2bc28d92fe58b83
yjgaia <yjdev88@gmail.com>
2025-05-13 13:56:06 +0900

    refactor: remove WalletAddressAvatar component and update exports

--- src/components/WalletAddressAvatar.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultAvatar } from "@commonmodule/app-components";
-import { getAddress } from "viem";
-
-export default class WalletAddressAvatar extends DefaultAvatar {
-  constructor(walletAddress: string) {
-    super(".wallet-avatar", getAddress(walletAddress));
-  }
-}
